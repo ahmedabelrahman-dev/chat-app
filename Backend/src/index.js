a00a654fbb80ee6777f701cede5650c72a98043b
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-09 19:44:07 +0400

    fixed uplaoding profile pic

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,12 +15,13 @@ const port = process.env.PORT;
 const app = express();
 
 // middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
+    origin: 'http://localhost:5173', // React app URL
+    credentials: true, // allow cookies/sessions if used
   })
 );
 
