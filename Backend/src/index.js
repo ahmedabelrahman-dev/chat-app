f6c6f823550f589f4bc9cb8bbe0d7a7cf6a432a6
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-09 20:39:31 +0400

    updated the mg size to uploade a profile pic

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT;
 const app = express();
 
 // middleware
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(
