b2085bb27a9368f62c71fd10270573793b7a8732
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-10 06:23:50 +0400

    added all the function needed for sending and reciving messages

--- Frontend/src/store/useChatStore.js
+++ Frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import toast from 'react-hot-toast';
 import instance from '../lib/axios';
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -31,6 +31,18 @@ export const useChatStore = create((set) => ({
       set({ isMessagesLoading: false });
     }
   },
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await instance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
   //todo: optimize later
-  setSelectedUsers: (selectedUser) => set({ selectedUser }),
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
